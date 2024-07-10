400aaabbab4455e171c4a7e4f0765f9d9a9a93ca
khvreddy1302 <kasaramharshavardhan@gmail.com>
2024-07-10 13:13:15 +0530

    static assets.js

---
+++ static assets.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path=require('path')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express();
+const publicDirectoryPath=path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+
+// app.com
+// app/help
+// app/about
+
+app.get('/', (req, res) => {
+    res.send("<h1>hello express</h1>");
+});
+
+app.get('/help', (req, res) => {
+    res.send([{
+        name:'andrew',
+        age:24,
+        salary:500000
+    },
+{
+    name:'sarah',
+    age:55,
+    salary:40000
+}]);
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>about the page</h1>');
+});
+
+app.get('/weather', (req, res) => {
+    res.send([{
+        forecast:'The weather is clear'
+    },{
+        location:'philadelohia'
+    }]);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}.`);
+});
