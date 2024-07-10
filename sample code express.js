15e6c465084115e12286e9bda3dbdc9b6f5da2a6
khvreddy1302 <kasaramharshavardhan@gmail.com>
2024-07-10 12:05:19 +0530

    Update sample code express.js

--- sample code express.js
+++ sample code express.js
@@ -1,7 +1,7 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// var app = (0, express_1.default)();
+// Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+var app = (0, express_1.default)();
 // app.com
 // app/help
 // app/about
